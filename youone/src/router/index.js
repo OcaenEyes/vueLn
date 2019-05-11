7795bde7f7afd0f3c6ec368c670275182484ce98
OcaenEyes <1450136519@qq.com>
2019-05-11 10:16:01 +0800

    Update index.js

--- youone/src/router/index.js
+++ youone/src/router/index.js
@@ -11,11 +11,6 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
-    },
-    {
-      path:'/blog',
-      name:'BlogHomePage',
-      component:BlogHomePage
-    }
+    }    
   ]
 })
