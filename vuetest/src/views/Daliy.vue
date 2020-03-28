a930a8ecfd30f8b173a29ed552c20439bf1f146f
OcaenEyes <1450136519@qq.com>
2020-03-28 12:08:47 +0800

    master

---
+++ vuetest/src/views/Daliy.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    每日一文
+    <!-- <div
+      v-for="(item ,index)in daliy"
+      :key="index"
+    >
+      <p>{{ item.id }}</p>
+    </div> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Daliy',
+  data () {
+    return {
+      msg: '123',
+      daliy: null
+    }
+  },
+
+  created () {
+    this.getDaliy()
+  },
+
+  methods: {
+    getDaliy () {
+      axios.get('http://localhost:8081/youone-default').then(
+
+        res => (this.daliy = res.data.content, console.log(this.daliy))
+      )
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
