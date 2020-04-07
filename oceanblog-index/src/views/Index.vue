a54f2b1d637166a083759cdd216af17110ff0999
OcaenEyes <1450136519@qq.com>
2020-04-07 21:59:43 +0800

    master

---
+++ oceanblog-index/src/views/Index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="index">
+    <Header></Header>
+    <Banner></Banner>
+    
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Banner from "../components/Banner"
+import Header from "../components/Header"
+export default {
+  name: 'Index',
+  components: {
+    Banner,
+    Header
+  }
+}
+</script>
+
+<style scoped>
+/* .index{
+  background-color: black;
+} */
+</style>
