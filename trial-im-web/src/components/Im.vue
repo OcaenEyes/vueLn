d0ed6c394d6d6cdb26d89017cd7bd997717a2243
OcaenEyes <1450136519@qq.com>
2020-12-03 19:12:04 +0800

    Update Im.vue

--- trial-im-web/src/components/Im.vue
+++ trial-im-web/src/components/Im.vue
@@ -1,27 +1,28 @@
 <template>
   <div>
     <div class="chat-window" style="height: 800px">
-      <div class="chat-title">与GZY的对话</div>
-      <div class="msg-list" style="height: 600px">
-        <div class="lchat-msg">
+      <div class="chat-title">与厉害的对话</div>
+      <div class="msg-list"  v-for="(msg,index) in messages" :key="index"> 
+
+        <div class="lchat-msg" :style="{display: msg.issend? none:''}">
           <img
-            class="avatar"
-            src="https://up.enterdesk.com/edpic_source/42/7d/72/427d72b831d61616098dbca1488bcb3c.jpg"
+            class="lavatar"
+            :src="msg.avatar"
           />
           <div class="lmsg">
-            <div class="lmsg-time">20201201 23:01</div>
-            <div class="lmsg-content">12121</div>
+            <div class="lmsg-time"><span style="color:#666;font-size: small;margin-right:10px">{{msg.username}}</span>{{msg.timestmp}}</div>
+            <div class="lmsg-content">{{msg.content}}</div>
           </div>
         </div>
 
-        <div class="rchat-msg">
+        <div class="rchat-msg" :style="{display: msg.issend? '':none}">
           <div class="rmsg">
-            <div class="rmsg-time">20201201 23:01</div>
-            <div class="rmsg-content">12121</div>
+            <div class="rmsg-time">{{msg.timestmp}}<span style="color:#666;font-size: small;margin-left:10px">{{msg.username}}</span></div>
+            <div class="rmsg-content">{{msg.content}}</div>
           </div>
           <img
-            class="avatar"
-            src="https://up.enterdesk.com/edpic_source/42/7d/72/427d72b831d61616098dbca1488bcb3c.jpg"
+            class="ravatar"
+            :src="msg.avatar"
           />
         </div>
       </div>
@@ -36,6 +37,54 @@
 <script>
 export default {
   name: "Im",
+  data() {
+    return {
+      none:"none",
+      messages:[
+        {
+          issend:true,
+          username:"巧克",
+          avatar:"https://up.enterdesk.com/edpic_source/42/7d/72/427d72b831d61616098dbca1488bcb3c.jpg",
+          content:"你好呀！！！",
+          timestmp:"20201202"
+        },
+        {
+          issend:false,
+          username:"厉害",
+          avatar:"https://up.enterdesk.com/edpic_source/42/7d/72/427d72b831d61616098dbca1488bcb3c.jpg",
+          content:"你好呀！！！",
+          timestmp:"20201202"
+        },
+        {
+          issend:true,
+          username:"巧克",
+          avatar:"https://up.enterdesk.com/edpic_source/42/7d/72/427d72b831d61616098dbca1488bcb3c.jpg",
+          content:"这是又一条消息",
+          timestmp:"20201202"
+        },
+        {
+          issend:false,
+          username:"厉害",
+          avatar:"https://up.enterdesk.com/edpic_source/42/7d/72/427d72b831d61616098dbca1488bcb3c.jpg",
+          content:"好的我收到了",
+          timestmp:"20201202"
+        },
+      ],
+    }
+  },
+  methods: {
+    // getMsg(){
+
+    // }
+    
+  },
+  mounted() {
+    // getMsg()
+    
+  },
+  created() {
+    
+  },
 };
 </script>
 
@@ -69,7 +118,6 @@ export default {
 }
 .lmsg-content {
   word-break: break-all;
-  max-width: 66%;
   background: #d3dce6;
   border-radius: 6px;
   text-align: center;
@@ -100,14 +148,18 @@ export default {
 .rmsg-content {
   background: #5fb948;
   word-break: break-all;
-  max-width: 66%;
   min-height: 36px;
   border-radius: 6px;
   text-align: center;
   line-height: 36px;
   padding: 0 10px 0 10px;
 }
-.avatar {
+.lavatar {
+  width: 30px;
+  height: 30px;
+  border-radius: 15px;
+}
+.ravatar {
   width: 30px;
   height: 30px;
   border-radius: 15px;
